687ebf3492b76613fe6f8ec4fbcdffc285fdc239
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-13 16:57:38 -0700

    updated the virat model part to include the changes in the dataset

--- src/i3d_virat.py
+++ src/i3d_virat.py
@@ -5,6 +5,9 @@ from keras.layers import Flatten
 from i3d_inception import Inception_Inflated3d
 from keras.callbacks import ModelCheckpoint
 from video_sequence import VideoframeGenerator
+from keras.models import load_model
+
+import pathlib
 def get_i3d_virat(input_shape,weights=None, dropout_prob=0.0):
     base_layer = Sequential()
     prev_model = Inception_Inflated3d(False,input_shape=input_shape, endpoint_logit=True, dropout_prob=dropout_prob)
@@ -16,15 +19,20 @@ def get_i3d_virat(input_shape,weights=None, dropout_prob=0.0):
     return base_layer
 
 def main():
-    dataset_root = "/workspaces/i3d_keras/dataset/TinyVIRAT/"
-    shape = (128, 112, 112, 3)
+    dataset_root = "/virat-vr/TinyVIRAT/"
+    shape = (256, 112, 112, 3)
+    checkpoint_path = "/virat-vr/tmp1.hdf5"
     training_generator = VideoframeGenerator(dataset_root, "tiny_train.json","train", "classes.txt")
     test_generator = VideoframeGenerator(dataset_root, "tiny_test.json","test", "classes.txt")
-    model = get_i3d_virat(shape, 'rgb_kinetics_only')
+    model = None
+    if pathlib.Path(checkpoint_path).exists():
+        model = load_model(checkpoint_path)
+    else:    
+        model = get_i3d_virat(shape, 'rgb_kinetics_only')
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    checkpointer = ModelCheckpoint(filepath='/workspaces/i3d_keras/dataset/TinyVIRAT/tmp1.hdf5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_best_only=False)
 
-    model.fit_generator(training_generator, epochs=1, use_multiprocessing=True, callbacks=[checkpointer] )
+    model.fit_generator(training_generator, epochs=1, use_multiprocessing=False, callbacks=[checkpointer] )
     #model.evaluate_generator(test_generator)
 if __name__ == '__main__':
     main()
