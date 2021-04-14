0c51e7a552359bdb23da155c4d410c0bcc99a736
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-14 05:28:50 -0700

    updated test script

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -10,15 +10,17 @@ def evaluate_model(model_filepath, test_dataset):
     trues = list()
     count = 0
     for batch, labels in test_dataset:
-        v = model.predict_on_batch(batch)
-        for y_t, y_p in zip(labels, v):
-            p = np.array([1 if z >=0.5 else 0 for z in y_p])
-            predictions.append(p)
-            trues.append(y_t)
-        count+=1
+        try:
+            count+=1
+            v = model.predict_on_batch(batch)
+            for y_t, y_p in zip(labels, v):
+                p = np.array([1 if z >=0.5 else 0 for z in y_p])
+                predictions.append(p)
+                trues.append(y_t)
+            print(count)
+        except:
+            print("Unable to predict on batch",count)
 
-        print(count)
-        break
     print(trues, predictions)
     f1_macro = f1_score(trues, predictions, average='macro')
     f1_micro = f1_score(trues, predictions, average='micro')
@@ -31,8 +33,10 @@ def iterate_over_sequence(sequence):
 
 
 def main():
-    model_path = '/workspaces/i3d_keras/models/i3d_bicubic_v1.h5'
-    dataset_root = "/workspaces/i3d_keras/dataset/TinyVIRAT/"
+    # model_path = '/workspaces/i3d_keras/models/i3d_bicubic_v1.h5'
+    # dataset_root = "/workspaces/i3d_keras/dataset/TinyVIRAT/"
+    model_path = '/virat-vr/models/tmp1_e8.hdf5'
+    dataset_root = "/virat-vr/TinyVIRAT/"
     test_generator = VideoframeGenerator(dataset_root, "tiny_test.json","test", "classes.txt", frames=256, batch_size=50, shuffle=False)
     #training_generator = VideoframeGenerator(dataset_root, "tiny_train.json","train", "classes.txt", frames=256, batch_size=5, shuffle=False)
     #iterate_over_sequence(training_generator)
