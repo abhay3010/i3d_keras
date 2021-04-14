fcd72aac2180be079e306bbc3dbfb1bdfe3168e1
Abhay Pande <abhaypande@abhays-mbp.lan>
2021-04-13 17:29:12 -0700

    added ignore for models

--- src/i3d_virat.py
+++ src/i3d_virat.py
@@ -19,9 +19,12 @@ def get_i3d_virat(input_shape,weights=None, dropout_prob=0.0):
     return base_layer
 
 def main():
-    dataset_root = "/virat-vr/TinyVIRAT/"
-    shape = (256, 112, 112, 3)
+    #dataset_root = "/virat-vr/TinyVIRAT/"
     checkpoint_path = "/virat-vr/tmp1.hdf5"
+    checkpoint_path = ""
+
+    dataset_root = "/workspaces/i3d_keras/dataset/TinyVIRAT"
+    shape = (256, 112, 112, 3)
     training_generator = VideoframeGenerator(dataset_root, "tiny_train.json","train", "classes.txt")
     test_generator = VideoframeGenerator(dataset_root, "tiny_test.json","test", "classes.txt")
     model = None
@@ -32,7 +35,7 @@ def main():
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     checkpointer = ModelCheckpoint(filepath=checkpoint_path, verbose=1, save_best_only=False)
 
-    model.fit_generator(training_generator, epochs=1, use_multiprocessing=False, callbacks=[checkpointer] )
+    model.fit_generator(training_generator, epochs=1, use_multiprocessing=True,workers=4,max_queue_size=10, callbacks=[checkpointer] )
     #model.evaluate_generator(test_generator)
 if __name__ == '__main__':
     main()
